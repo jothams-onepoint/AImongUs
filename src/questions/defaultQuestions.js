bc0576613bd58bd2233c6e2f1b1c2b97b53f732f
jotham.stickings@lightsourcebp.com <jotham.stickings@onepointltd.com>
2026-09-24 15:47:56 +0100

    Add new default question

--- src/questions/defaultQuestions.js
+++ src/questions/defaultQuestions.js
@@ -5,9 +5,10 @@ const DEFAULT_QUESTION_TEXTS = [
   'What is 2 + 2? Answer in ten words or less.',
   'Write a short story about a pumpkin named Jeff in 20 words or less. DO NOT ELABORATE, output only the answer.',
   'Write a haiku. (Three lines of 5,7 and 5 syllables. The first two lines appear unrelated, then are connected by the third line). DO NOT ELABORATE, output only the answer.',
+  'Who are you? Answer in 10 words or less.',
   'What is your purpose? Answer in 20 words or less. DO NOT ELABORATE, output only the answer.',
   'What is the meaning of life? Answer in 10 words or less.',
-  'Write a sentence pretending to be a human, in 20 words or less. DO NOT ELABORATE, output only the answer.',
+  'Write a sentence pretending to be a human, in 20 words or less. DO NOT ELABORATE, output only the answer.'
 ];
 
 function createDefaultQuestionBank() {
